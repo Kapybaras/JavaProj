3daaac28d7cce57c0d5900199d0b30b52bcbd280
Callum <cmjomes@gmail.com>
2026-03-09 14:08:09 +0000

    Initial commit

---
+++ src/main/java/uk/ac/bangor/cs/a3/academigymraeg/BootProject1Application.java
@@ -0,0 +1,13 @@
+package uk.ac.bangor.cs.a3.academigymraeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/bangor/cs/a3/academigymraeg/BootProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.bangor.cs.a3.academigymraeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
